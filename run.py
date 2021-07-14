d86f77fdbba1c8a8048c907a79b7e71201c89c98
Eb0nY-April14 <laratemi14@gmail.com>
2021-07-14 21:04:39 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,1 +1,12 @@
-# python code goes here
+
+choice = input("""
+Please Enter the operator you need for your calculation:
+a for Add
+b for Subtract
+c for Multiply
+d for Divide
+e for Modulo
+f for Power(Exponent)
+g for Square Root
+""")
+
