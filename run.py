20cd53a69ffd4f07cec640b32dec5f56d234b922
Eb0nY-April14 <laratemi14@gmail.com>
2021-07-14 21:35:01 +0000

    Add a function to calculate the subtraction of two numbers

--- run.py
+++ run.py
@@ -13,20 +13,30 @@ g for Square Root
 
 def calculate_operation():
     NUM_CONSTANT = 0.5
-    #invalid_operator_result = 0
+    invalid_operator_result = 0.0
     if choice == 'g':
         num = float(input("Enter a number: "))
         result = num ** NUM_CONSTANT
         return result
-    if choice == 'a':
+    else:
         num_1 = float(input("Enter your first number: "))
-        num_2 = float(input("Enter your second number: "))
-        result = num_1 + num_2
-        return result
+        num_2 = float(input("Enter your second number: "))    
+        if choice == 'a':
+        #num_1 = float(input("Enter your first number: "))
+        #num_2 = float(input("Enter your second number: "))
+            result = num_1 + num_2
+            return result
+        elif choice == 'b':
+        #num_1 = float(input("Enter your first number: "))
+        #num_2 = float(input("Enter your second number: "))
+            result = num_1 - num_2
+            return result
+        else:
+            return invalid_operator_result    
 
 
 answer = calculate_operation() 
-print(f"The Result of your calculation is: {answer}" )       
+print(f"The Result of your calculation is: {answer}")       
 
 
 
