542f33fcabee85bca1dc95cc78e9405231734dba
Eb0nY-April14 <laratemi14@gmail.com>
2021-07-19 18:57:28 +0000

    Refactor the request_any_number() function to accept user input and use the try except ValueError

--- run.py
+++ run.py
@@ -27,33 +27,33 @@ def calculate_operation():
         # validnum_2 = request_any_number()
         # print("test", validnum_2)
         # print(f"The data type of {validnum_2} is {(type(validnum_2))}")
-        if choice == 'a':
+        if choice == "a":
             print("Doing the Add Calculation...\n")
             result = num_1 + num_2
             print(result)
             print("Add Calculation completed Successfully.\n")
             return result
-        elif choice == 'b':
+        elif choice == "b":
             print("Doing the Subtract Calculation...\n")
             result = num_1 - num_2
             print("Subtract Calculation completed Successfully.\n")
             return result
-        elif choice == 'c':
+        elif choice == "c":
             print("Doing the Multiply Calculation...\n")
             result = num_1 * num_2
             print("Multiply Calculation completed Successfully.\n")
             return result
-        elif choice == 'd':
+        elif choice == "d":
             print("Doing the Division Calculation...\n")
             result = num_1 / num_2
             print("Division Calculation completed Successfully.\n")
             return result
-        elif choice == 'e':
+        elif choice == "e":
             print("Doing the Modulo Calculation...\n")
             result = num_1 % num_2
             print("Modulo Calculation completed Successfully.\n")
             return result
-        elif choice == 'f':
+        elif choice == "f":
             print("Doing the Exponent Calculation...\n")
             result = num_1 ** num_2
             print("Exponent Calculation completed Successfully.\n")
@@ -62,17 +62,18 @@ def calculate_operation():
             return "Null. You entered an invalid operator"
 
 
-def request_any_number():
-    num = input("Enter a number:\n")
-    print(f"The number entered by the user is {num}")
-    print(f"The data type of number entered by the user is {type(num)}")
-    val_number = validate_number(num)
-    if validate_number(num):
-        print(f"The validated number is: {val_number}")
-        print(type(val_number))  # num is a string not float
-        return val_number
-    else:
-        request_any_number()
+# def 
+#    while True:
+#    num = input("Enter a number:\n")
+#    print(f"The number entered by the user is {num}")
+#    print(f"The data type of number entered by the user is {type(num)}")
+#    val_number = validate_number(num)
+#    if validate_number(num):
+#        print(f"The validated number is: {val_number}")
+#        print(type(val_number))  # num is a string not float
+#        return val_number
+#    else:
+#        request_any_number()
     # if validate_number(val_number):
 
     # if num:
@@ -84,17 +85,20 @@ def request_any_number():
     #        request_any_number()
 
 
-def validate_number(number):
-    try:
-        val_num = float(number)
-        print("The input entered by the user is Ok!")
-        print(type(val_num))
-        if type(val_num) == float:
-            return val_num
-    except ValueError:
-        print("""Invalid input, NOT a number,
-            Please try again!""")
-        print(number)
+def request_any_number(pos):
+    while True:
+        try:
+            num = float(input(f"Enter {pos} number:\n"))
+            return num
+        # val_num = float(number)
+        # print("The input entered by the user is Ok!")
+        # print(type(val_num))
+        # if type(val_num) == float:
+        #     return val_num
+        except ValueError:
+            print("Invalid input - NOT a number. \n"
+                  "Please try again!")
+        # print(number)
 
 
 print("Welcome to our text-based Calculator")
