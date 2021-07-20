9677021b63a20750f5d22e3fbd9d50f47a11777f
Eb0nY-April14 <laratemi14@gmail.com>
2021-07-20 16:59:52 +0000

    Add a while loop to run the program again if a user wants to and personalize the app with the user's name

--- run.py
+++ run.py
@@ -1,61 +1,56 @@
 def choose_operator():
     global choice
     choice = input(
-                   "Please Enter the operator you need for your calculation:\n"
-                   "a for Add\n"
-                   "b for Subtract\n"
-                   "c for Multiply\n"
-                   "d for Divide\n"
-                   "e for Modulo\n"
-                   "f for Power(Exponent)\n"
-                   "g for Square Root\n")
+                    """Please Enter the operator you need for your calculation:\n """
+                    "a for Add\n"
+                    "b for Subtract\n"
+                    "c for Multiply\n"
+                    "d for Divide\n"
+                    "e for Modulo\n"
+                    "f for Power(Exponent)\n"
+                    "g for Square Root\n")
+    return choice
 
 
 def calculate_operation():
+    choose_operator()
     NUM_CONSTANT = 0.5
     if choice == "g":
         print("Doing the Square Root Calculation...\n")
         sqrt_num = request_any_number("a")
-        # val_num = validate_number(num)
-        # if validate_number(val_num):
         result = sqrt_num ** NUM_CONSTANT
         print("Square Root Calculation completed Successfully.\n")
         return result
     else:
-        num_1 = request_any_number("the first")
-        num_2 = request_any_number("the second")
-        # print("test", validnum_1)
-        # print(f"The data type of {validnum_1} is {(type(validnum_1))}")
-        # validnum_2 = request_any_number()
-        # print("test", validnum_2)
-        # print(f"The data type of {validnum_2} is {(type(validnum_2))}")
-        if choice == "a":
+        num_1 = request_any_number("the_first")
+        num_2 = request_any_number("the_second")
+        my_choice = choice
+        if my_choice == "a":
             print("Doing the Add Calculation...\n")
             result = num_1 + num_2
-            print(result)
             print("Add Calculation completed Successfully.\n")
             return result
-        elif choice == "b":
+        elif my_choice == "b":
             print("Doing the Subtract Calculation...\n")
             result = num_1 - num_2
             print("Subtract Calculation completed Successfully.\n")
             return result
-        elif choice == "c":
+        elif my_choice == "c":
             print("Doing the Multiply Calculation...\n")
             result = num_1 * num_2
             print("Multiply Calculation completed Successfully.\n")
             return result
-        elif choice == "d":
+        elif my_choice == "d":
             print("Doing the Division Calculation...\n")
             result = num_1 / num_2
             print("Division Calculation completed Successfully.\n")
             return result
-        elif choice == "e":
+        elif my_choice == "e":
             print("Doing the Modulo Calculation...\n")
             result = num_1 % num_2
             print("Modulo Calculation completed Successfully.\n")
             return result
-        elif choice == "f":
+        elif my_choice == "f":
             print("Doing the Exponent Calculation...\n")
             result = num_1 ** num_2
             print("Exponent Calculation completed Successfully.\n")
@@ -64,52 +59,25 @@ def calculate_operation():
             return "Null. You entered an invalid operator"
 
 
-# def 
-#    while True:
-#    num = input("Enter a number:\n")
-#    print(f"The number entered by the user is {num}")
-#    print(f"The data type of number entered by the user is {type(num)}")
-#    val_number = validate_number(num)
-#    if validate_number(num):
-#        print(f"The validated number is: {val_number}")
-#        print(type(val_number))  # num is a string not float
-#        return val_number
-#    else:
-#        request_any_number()
-    # if validate_number(val_number):
-
-    # if num:
-    #    if validate_number(num):
-    #        print(f"The validated number is: {val_number}")
-    #        print(type(val_number))  # num is a string not float
-    #        return val_number
-    #    else:
-    #        request_any_number()
-
-
 def request_any_number(pos):
     while True:
         try:
             num = float(input(f"Enter {pos} number:\n"))
             return num
-        # val_num = float(number)
-        # print("The input entered by the user is Ok!")
-        # print(type(val_num))
-        # if type(val_num) == float:
-        #     return val_num
         except ValueError:
             print("Invalid input - NOT a number. \n"
                   "Please try again!")
-        # print(number)
 
 
-print("Welcome to our text-based Calculator")
+your_name = input("Please Enter your name: \n")
+print(f"Welcome {your_name} to the Calculator5000")
 answer = calculate_operation()
-print(f"Result of your calculation is {answer}")
+print(f"The Final Result of your calculation is {answer}")
 
 response = input("Would you like to Perform another Calculation y/n? ")
-if response == "y" or response == "Y":
-    print(response)
-# else:
-#    break
-    # print(response)
+while response == "y" or response == "Y":
+    my_new_answer = calculate_operation()
+    print(f"The Final Result of your calculation is {my_new_answer}\n")
+    response = input("Would you like to Perform another Calculation y/n? ")
+else:
+    print(f"Thanks for using the Calculator5000 {your_name}, Goodbye!")
