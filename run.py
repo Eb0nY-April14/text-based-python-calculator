699198a065948537813546b20c4e37363c017eb9
Eb0nY-April14 <laratemi14@gmail.com>
2021-07-23 10:33:02 +0000

    Add clarity to the code using docstrings

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
 def choose_operator():
+    """
+    Requests input for choice of operator from the user,
+    displays meaningful feedback to the user if invalid
+    choice is made.
+    """
     global choice
     choice = input(
                     """Please Enter the operator you need
@@ -14,6 +19,10 @@ def choose_operator():
 
 
 def calculate_operation():
+    """
+    Performs the relevant calculation based on the user's
+    choice and returns the result to the calling code.
+    """
     choose_operator()
     NUM_CONSTANT = 0.5
     if choice == "g":
@@ -61,6 +70,11 @@ def calculate_operation():
 
 
 def request_any_number(pos):
+    """
+    Requests for number inputs from the user and handles the
+    exceptions and displays meaningful feedback to the user on
+    the source of the error and how to handle it.
+    """
     while True:
         try:
             num = float(input(f"Enter {pos} number:\n"))
@@ -70,11 +84,21 @@ def request_any_number(pos):
                   "Please try again!")
 
 
+"""
+Accept user name and displays a personalised welcome message
+to the user.
+"""
 your_name = input("Please Enter your name: \n")
 print(f"Welcome {your_name} to the Calculator5000")
 answer = calculate_operation()
 print(f"The Final Result of your calculation is {answer}")
 
+
+"""
+Asks the user if he wants to perform another operation and
+if yes, repeats the app all over aagain and if no, displays
+the final result and exits.
+"""
 response = input("Would you like to Perform another Calculation y/n?\n")
 while response == "y" or response == "Y":
     my_new_answer = calculate_operation()
